582796bc17863f9322c36151629d5ffca1de5b82
xuzhaocheng <zhaochengxu1991@gmail.com>
2014-11-26 00:12:16 +0800

    Initial Commit

---
+++ RemoteMusicPlayerControllerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RemoteMusicPlayerControllerDemo
+//
+//  Created by xuzhaocheng on 14/11/26.
+//  Copyright (c) 2014年 xuzhaocheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RemoteMusicPlayerControllerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RemoteMusicPlayerControllerDemo
+//
+//  Created by xuzhaocheng on 14/11/26.
+//  Copyright (c) 2014年 xuzhaocheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
